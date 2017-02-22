09aa29ce0cbd6caa62ef128e519e027234f88ef2
CodyAnagnost <codyanagnost@hotmail.com>
2017-02-22 15:15:21 -0500

    checklowanhighrate

--- CS_Chapter3/CheckLowAndHighRate/Program.cs
+++ CS_Chapter3/CheckLowAndHighRate/Program.cs
@@ -9,6 +9,33 @@ namespace CheckLowAndHighRate
     {
         static void Main(string[] args)
         {
+            const double HIGH = 49.99;
+
+            const double LOW = 5.65;
+
+            double HourlyPayRate;
+
+
+            Console.Write("Enter your hourly pay rate:");
+
+            HourlyPayRate = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+
+
+            if (HourlyPayRate > HIGH) ;
+
+            Console.WriteLine("The amount entered is to much");
+
+
+    else
+
+    if (HourlyPayRate < LOW) ;
+            
+    Console.WriteLine("The amount you entered is to small");
+
         }
     }
 }
